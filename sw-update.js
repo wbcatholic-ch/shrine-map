f04eee8776b101c771772ec9aded831314817dc4
wbcatholic-ch <bong0219@gmail.com>
2026-06-11 11:52:14 +0900

    Add files via upload

--- sw-update.js
+++ sw-update.js
@@ -3,8 +3,8 @@
   'use strict';
   if(window.__APP_CACHE_LIFECYCLE_GUARD__) return;
   window.__APP_CACHE_LIFECYCLE_GUARD__ = true;
-  var APP_VERSION = 'WebView-Clean-17';
-  var SW_BUILD_VERSION = 'WebView-Clean-17';
+  var APP_VERSION = 'WebView-Clean-18';
+  var SW_BUILD_VERSION = 'WebView-Clean-18';
   window.APP_VERSION = APP_VERSION;
 
 
