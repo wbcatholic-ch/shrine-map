87de9416cf9e3b56bffb88aaec74cf6e00722659
wbcatholic-ch <bong0219@gmail.com>
2026-06-22 12:36:27 +0900

    Add files via upload

--- js/cover-common.js
+++ js/cover-common.js
@@ -3,7 +3,7 @@
 (function(){
   if(window.__APP_FONT_SCALE_GUARD__) return;
   window.__APP_FONT_SCALE_GUARD__=true;
-  var QA_URL="qa-firebase.html?v=V8-1-14-103-XLSX-INFO-SYNC";
+  var QA_URL="qa-firebase.html?v=V8-1-14-104-XLSX-SHRINE-DATA-SYNC";
   var FONT_KEY='prayer_font_size';
   var BASE=16;
   var FONT_SIZES=[13,14,15,16,17,18,19,20,21,22,24,26,28,30];
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_VERSION = 'catholic-way-V8-1-14-103-XLSX-INFO-SYNC';
-const ASSET_VERSION = 'V8-1-14-103-XLSX-INFO-SYNC';
+const CACHE_VERSION = 'catholic-way-V8-1-14-104-XLSX-SHRINE-DATA-SYNC';
+const ASSET_VERSION = 'V8-1-14-104-XLSX-SHRINE-DATA-SYNC';
 function withVersion(path) {
   return path + '?v=' + ASSET_VERSION;
 }
