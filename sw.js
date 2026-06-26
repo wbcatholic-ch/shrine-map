024296a74ffd537ca1025804d5c7fc46d21fd46e
wbcatholic-ch <bong0219@gmail.com>
2026-06-26 12:58:34 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_VERSION = 'catholic-way-V8-1-14-232_diocese_province_info_source_exact_spacing_full';
-const ASSET_VERSION = 'V8-1-14-232_diocese_province_info_source_exact_spacing_full';
+const CACHE_VERSION = 'catholic-way-V8-1-14-233_diocese_vicariate_key_features_restore_full';
+const ASSET_VERSION = 'V8-1-14-233_diocese_vicariate_key_features_restore_full';
 function withVersion(path) {
   return path + '?v=' + ASSET_VERSION;
 }
