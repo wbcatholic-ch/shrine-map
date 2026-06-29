a3f03d6c695ac3283d3bf107b034ebe810bb590b
wbcatholic-ch <bong0219@gmail.com>
2026-06-29 17:29:13 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_VERSION = 'catholic-way-V8-1-14-316';
-const ASSET_VERSION = 'V8-1-14-316';
+const CACHE_VERSION = 'catholic-way-V8-1-14-317';
+const ASSET_VERSION = 'V8-1-14-317';
 function withVersion(path) {
   return path + '?v=' + ASSET_VERSION;
 }
