67bd50aaa84034c02820c72c45d16feadcd6d978
wbcatholic-ch <bong0219@gmail.com>
2026-07-04 03:47:08 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE_VERSION = 'catholic-way-V8-1-14-541';
-const ASSET_VERSION = 'V8-1-14-541';
+const CACHE_VERSION = 'catholic-way-V8-1-14-542';
+const ASSET_VERSION = 'V8-1-14-542';
 
-/* V8-1-14-541: service worker cache strategy overview.
+/* V8-1-14-542: service worker cache strategy overview.
    - APP_SHELL: first-screen and internal helper files.
    - HTML navigation: networkFirst, then cached index fallback.
    - Versioned/static assets: cacheFirst.
