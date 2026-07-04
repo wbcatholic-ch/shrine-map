b8be83df57d12f28c9b38ba311055b2997866967
wbcatholic-ch <bong0219@gmail.com>
2026-07-04 09:55:28 +0900

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,7 +1,6 @@
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
 
-
 firebase.initializeApp({
   apiKey:            'AIzaSyCGUwhWXTgc09ghEtOcvinpQKrr577zAhM',
   authDomain:        'catholic-6c7d8.firebaseapp.com',
