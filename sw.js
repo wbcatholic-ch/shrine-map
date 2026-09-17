8370e18a9944430559787867ffb90d5b477bdfb0
wbcatholic-ch <bong0219@gmail.com>
2026-09-17 16:37:09 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_VERSION = 'catholic-way-V8-1-14-714';
-const ASSET_VERSION = 'V8-1-14-714';
+const CACHE_VERSION = 'catholic-way-V8-1-14-715';
+const ASSET_VERSION = 'V8-1-14-715';
 
 /* V8-1-14-693: service worker cache strategy overview.
    - APP_SHELL: first-screen and internal helper files.
